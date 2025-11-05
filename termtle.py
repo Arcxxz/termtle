84cbb47b7cf7ed32bf1f81b2d203518eb88dceff
Arcxxz <160338496+Arcxxz@users.noreply.github.com>
2025-11-05 13:40:16 -0500

    added files via upload

---
+++ termtle.py
@@ -0,0 +1,167 @@
+import time
+import curses
+
+def main(stdscr):
+
+    curses.start_color()
+    curses.use_default_colors()
+
+    curses.init_pair(1, curses.COLOR_WHITE, -1) 
+
+    curses.curs_set(0)
+
+    while True:
+        
+        stdscr.clear()
+
+        stdscr.addstr("      ___________________________\n")
+        stdscr.addstr("     (                           )\n")
+        stdscr.addstr("    .-'-------------------------'-.\n")
+        stdscr.addstr("   /                               \\\n")
+        stdscr.addstr("  /~-_-~-_-~-_-~-_-~-_-~-_-~-_-~-_-~\\\n")
+        stdscr.addstr(" ;       o                    O      ;\n")
+        stdscr.addstr(";                   O   o         O   ;\n")
+        stdscr.addstr("|    O     o                 o        |\n")
+        stdscr.addstr("|               O     o           o   |\n")
+        stdscr.addstr("|       O                    o        |\n")
+        stdscr.addstr(";   o        .----.  __             O ;\n")
+        stdscr.addstr(" ;          /______\/'')  O      o   ;\n")
+        stdscr.addstr("  \\..,,...,..UU.,UU,,.,...,..,,...,./\n")
+        stdscr.addstr("   '-.___________________________.-'\n")
+        stdscr.addstr("     (___________________________)\n")
+
+        stdscr.refresh()
+
+        time.sleep(1)
+
+        stdscr.clear()
+
+        stdscr.addstr("      ___________________________\n")
+        stdscr.addstr("     (                           )\n")
+        stdscr.addstr("    .-'-------------------------'-.\n")
+        stdscr.addstr("   /                               \\\n")
+        stdscr.addstr("  /~-_-~-_-~-_-~-_-~-_-~-_-~-_-~-_-~\\\n")
+        stdscr.addstr(" ;                                   ;\n")
+        stdscr.addstr(";     o                         O     ;\n")
+        stdscr.addstr("|            o            O           |\n")
+        stdscr.addstr("|         O            o              |\n")
+        stdscr.addstr("|  o           o               O      |\n")
+        stdscr.addstr(";      O     .----.  __    o          ;\n")
+        stdscr.addstr(" ;     o    /______\/'')        O    ;\n")
+        stdscr.addstr("  \\..,,...,..UU.,UU,,.,...,..,,...,./\n")
+        stdscr.addstr("   '-.___________________________.-'\n")
+        stdscr.addstr("     (___________________________)\n")
+
+        stdscr.refresh()
+
+        time.sleep(1)
+        
+        stdscr.clear()
+
+        stdscr.addstr("      ___________________________\n")
+        stdscr.addstr("     (                           )\n")
+        stdscr.addstr("    .-'-------------------------'-.\n")
+        stdscr.addstr("   /                               \\\n")
+        stdscr.addstr("  /~-_-~-_-~-_-~-_-~-_-~-_-~-_-~-_-~\\\n")
+        stdscr.addstr(" ;         O              o          ;\n")
+        stdscr.addstr(";   o             o                   ;\n")
+        stdscr.addstr("|                           O     o   |\n")
+        stdscr.addstr("|           O                         |\n")
+        stdscr.addstr("|                o               O    |\n")
+        stdscr.addstr(";            .----.  __               ;\n")
+        stdscr.addstr(" ;      o   /______\/'')             ;\n")
+        stdscr.addstr("  \\..,,...,..UU.,UU,,.,...,..,,...,./\n")
+        stdscr.addstr("   '-.___________________________.-'\n")
+        stdscr.addstr("     (___________________________)\n")
+
+        stdscr.refresh()
+
+        time.sleep(1)
+
+        stdscr.clear()
+
+        stdscr.addstr("      ___________________________\n")
+        stdscr.addstr("     (                           )\n")
+        stdscr.addstr("    .-'-------------------------'-.\n")
+        stdscr.addstr("   /                               \\\n")
+        stdscr.addstr("  /~-_-~-_-~-_-~-_-~-_-~-_-~-_-~-_-~\\\n")
+        stdscr.addstr(" ;       o                    O      ;\n")
+        stdscr.addstr(";                   O   o         O   ;\n")
+        stdscr.addstr("|    O     o                 o        |\n")
+        stdscr.addstr("|               O     o           o   |\n")
+        stdscr.addstr("|       O                    o        |\n")
+        stdscr.addstr(";   o        .----.  __             O ;\n")
+        stdscr.addstr(" ;          /______\/'')  O      o   ;\n")
+        stdscr.addstr("  \\..,,...,..UU.,UU,,.,...,..,,...,./\n")
+        stdscr.addstr("   '-.___________________________.-'\n")
+        stdscr.addstr("     (___________________________)\n")
+
+        stdscr.refresh()
+
+        time.sleep(1)
+
+        stdscr.clear()
+
+        stdscr.addstr("      ___________________________\n")
+        stdscr.addstr("     (                           )\n")
+        stdscr.addstr("    .-'-------------------------'-.\n")
+        stdscr.addstr("   /                               \\\n")
+        stdscr.addstr("  /_-~-_-~-_-~-_-~-_-~-_-~-_-~-_-~-_\\\n")
+        stdscr.addstr(" ;                                   ;\n")
+        stdscr.addstr(";     o                         O     ;\n")
+        stdscr.addstr("|            o            O           |\n")
+        stdscr.addstr("|         O            o              |\n")
+        stdscr.addstr("|  o           o               O      |\n")
+        stdscr.addstr(";      O     .----.  __    o          ;\n")
+        stdscr.addstr(" ;     o    /______\/'')        O    ;\n")
+        stdscr.addstr("  \\..,,...,..UU.,UU,,.,...,..,,...,./\n")
+        stdscr.addstr("   '-.___________________________.-'\n")
+        stdscr.addstr("     (___________________________)\n")
+
+        stdscr.refresh()
+
+        time.sleep(1)
+        
+        stdscr.clear()
+
+        stdscr.addstr("      ___________________________\n")
+        stdscr.addstr("     (                           )\n")
+        stdscr.addstr("    .-'-------------------------'-.\n")
+        stdscr.addstr("   /                               \\\n")
+        stdscr.addstr("  /~-_-~-_-~-_-~-_-~-_-~-_-~-_-~-_-~\\\n")
+        stdscr.addstr(" ;         O              o          ;\n")
+        stdscr.addstr(";   o             o                   ;\n")
+        stdscr.addstr("|                           O     o   |\n")
+        stdscr.addstr("|           O                         |\n")
+        stdscr.addstr("|                o               O    |\n")
+        stdscr.addstr(";            .----.  __               ;\n")
+        stdscr.addstr(" ;      o   /______\/--)             ;\n")
+        stdscr.addstr("  \\..,,...,..UU.,UU,,.,...,..,,...,./\n")
+        stdscr.addstr("   '-.___________________________.-'\n")
+        stdscr.addstr("     (___________________________)\n")
+
+        stdscr.refresh()
+
+        time.sleep(1)
+
+
+curses.wrapper(main)
+
+#Anim Base:
+
+#Frame1
+#print("      ___________________________")
+#print("     (                           )")
+#print("    .-'-------------------------'-.")
+#print("   /                               \\")
+#print("  /~-_-~-_-~-_-~-_-~-_-~-_-~-_-~-_-~\\")
+#print(" ;                                   ;")
+#print(";                                     ;")
+#print("|                                     |")
+#print("|                                     |")
+#print("|                                     |")
+#print(";                                     ;")
+#print(" ;                                   ;")
+#print("  \\                                 /")
+#print("   '-.___________________________.-'")
+#print("     (___________________________)")
